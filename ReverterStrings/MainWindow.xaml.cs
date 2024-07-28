6ff7cd2c7a40f917093a39869f0d786edee16de3
Aliks175 <lesh-work@yandex.ru>
2024-07-28 19:49:34 +0300

    Первая проверка

--- ReverterStrings/MainWindow.xaml.cs
+++ ReverterStrings/MainWindow.xaml.cs
@@ -1,28 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ReverterStrings
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        string[] word;
+        string tempText;
+
         public MainWindow()
         {
             InitializeComponent();
+            OutTextSpliter.ItemsSource = new string[] { "Результат..." };
+        }
+
+        private string[] SplitText(string text)
+        {
+            char[] separators = new char[] { ' ', '.', ',', '?', '!' };
+            word = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return word;
+        }
+
+        private string Reverse(string text)
+        {
+            string resultString = null;
+            word = SplitText(text);
+            for (int i = word.Length; i > 0; i--)
+            {
+                if (word[i - 1] != null)
+                {
+                    resultString += word[i - 1] + ' ';
+                }
+            }
+            return resultString;
+        }
+
+
+        private void ClearButtonSplit(object sender, RoutedEventArgs e)
+        {
+            InputTextSpliter.Text = "Введите Ваш текст";
+            OutTextSpliter.ItemsSource = new string[] { "Результат..." };
+        }
+
+        private void ClearButtonRevert(object sender, RoutedEventArgs e)
+        {
+            InputTextRevert.Text = "Введите Ваш текст";
+            OutTextRevert.Content = "Результат...";
+        }
+
+        private void ButtonRevert(object sender, RoutedEventArgs e)
+        {
+            tempText = InputTextRevert.Text;
+            OutTextRevert.Content = Reverse(tempText);
+        }
+
+        private void ButtonSplit(object sender, RoutedEventArgs e)
+        {
+            tempText = InputTextSpliter.Text;
+            string[] strings = SplitText(tempText);
+            OutTextSpliter.ItemsSource = strings;
         }
     }
 }
